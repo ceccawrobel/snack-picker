d90b6716500da5024a19f30d3cb9e8239a5aa10e
f wrobel <ceccawrobel@gmail.com>
2019-06-19 16:54:11 -0700

    define variables within js front-end logic for each of the five form inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 
 $(document).ready(function() {
+  var hungry = $("select#hungry").val();
+  var protein = $("select#protein").val();
+  var sweet = $("select#sweet").val();
+  var salty = $("select#salty").val();
+  var bedtime = $("select#bedtime").val();
+
   $("#snackQuestions").submit(function(event) {
     event.preventDefault();
 
