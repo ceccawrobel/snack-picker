35efb9dad6235f9725e46b51bf8e9edf0d374cf0
f wrobel <ceccawrobel@gmail.com>
2019-06-19 17:47:46 -0700

    add more specific results based on js if/else statements

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,27 @@ $(document).ready(function() {
 
   $("#snackQuestions").submit(function(event) {
     event.preventDefault();
-    alert("inputs are " + hungry + ", " + protein + ", " + sweet + ", " + salty + ", " + bedtime);
-    if (hungry === "yes") {
+    console.log("inputs are " + hungry + ", " + protein + ", " + sweet + ", " + salty + ", " + bedtime);
+    if (hungry === "no" && bedtime === "yes") {
+      $("#nosnack").show();
+    } else if (protein === "no") {
       $("#sacha").show();
+    } else if (hungry === "no" && sweet === "yes" && salty === "yes") {
+      $("#licorice").show();
+    } else if (hungry === "no" && sweet === "no" && salty === "no") {
+      $("#celery").show();
+    } else if (hungry === "no" && sweet === "yes" && salty === "no") {
+      $("#cinnamon").show();
+    } else if (hungry === "no" && sweet === "no" && salty === "yes") {
+      $("#seaweed").show();
+    } else if (hungry === "yes" && protein === "yes") {
+      $("#pineapple").show();
+      $("#granola").show();
+    } else if (hungry === "no") {
+      $("#licorice").show();
+      $("#celery").show();
+      $("#seaweed").show();
+      $("#cinnamon").show();
     } else {
     $(".result").show();
   }
